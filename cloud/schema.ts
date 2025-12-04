22879c524684535272c0ca13997f72b761990274
flamingo <120697898+scorpionswedasy@users.noreply.github.com>
2025-12-04 14:20:40 -0800

    Update schema.ts

--- cloud/schema.ts
+++ cloud/schema.ts
@@ -1,17 +1,1 @@
-export const schemaDefinitions = [
-  {
-    className: 'TestObject',
-    fields: {
-      beforeSave: { type: 'Boolean', defaultValue: false },
-      additionalData: { type: 'String' },
-    },
-    classLevelPermissions: {
-      find: { '*': true },
-      count: { '*': true },
-      get: { '*': true },
-      update: { '*': true },
-      create: { '*': true },
-      delete: { '*': true },
-    },
-  },
-];
+export const schemaDefinitions = [];
