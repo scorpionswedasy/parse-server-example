82d27123f8c374b41c7a1a8a7e1bf9a80b303ffb
flamingo <120697898+scorpionswedasy@users.noreply.github.com>
2025-12-04 14:44:29 -0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -5,6 +5,7 @@ export const config = {
     process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb://localhost:27017/dev',
   cloud: () => import('./cloud/main.js'),
   appId: process.env.APP_ID || 'APP1',
+  clientKey: process.env.CLIENT_KEY || 'CLIENT1',
   masterKey: process.env.MASTER_KEY || 'MASTER1', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://parse-server-example-vs24.onrender.com/parse', // Don't forget to change to https if needed
   liveQuery: {
