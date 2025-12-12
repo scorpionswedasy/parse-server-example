b6fed4a0b7438aa24af9da1142e6c2f2249f394b
flamingo <120697898+scorpionswedasy@users.noreply.github.com>
2025-12-11 17:28:20 -0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,22 +1,18 @@
-import path from 'path';
+import path = require('path');
+const __dirname = path.resolve();
 
 export const config = {
   databaseURI: process.env.DATABASE_URI || 'mongodb://localhost:27017/dev',
-  cloud: path.join(__dirname, 'dist/cloud/main.js'),
+  cloud: path.join(__dirname, 'dist/cloud/main.js'), // بعد التحويل
   appId: process.env.APP_ID || 'APP1',
   clientKey: process.env.CLIENT_KEY || 'CLIENT1',
   masterKey: process.env.MASTER_KEY || 'MASTER1',
-  serverURL: process.env.SERVER_URL || 'https://parse-server-example-vs24.onrender.com/parse',
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
   allowClientClassCreation: true,
   enforcePrivateUsers: false,
   allowClientClassUpdate: true,
   enableAnonymousUsers: true,
   liveQuery: {
     classNames: ['Posts', 'Comments']
-  },
-  schema: {
-    definitions: require(path.join(__dirname, 'dist/cloud/schema.js')).schemaDefinitions,
-    lockSchemas: false,
-    strict: false
   }
 };
